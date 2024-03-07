9b933ed88efeac4a6ec648f5709937fea7a5070f
Raghavendra Mirajkar <raghavendramirajkar@Raghavendras-MacBook-Air.local>
2024-03-07 15:24:13 +0530

    Completed the app

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,53 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
 
+    var audioPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
+    func playSound(_ sound : String){
+        let soundURL = NSURL(fileURLWithPath: Bundle.main.path(forResource: sound, ofType: "wav")!)
+            do{
+                audioPlayer = try AVAudioPlayer(contentsOf: soundURL as URL)
+            }catch {
+                print("there was some error. The error was \(error)")
+            }
+            audioPlayer.play()
+    }
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound("C")
+    }
+    
+    @IBAction func keyPressedD(_ sender: UIButton) {
+        playSound("D")
+    }
+    
+    @IBAction func keyPressedE(_ sender: UIButton) {
+        playSound("E")
+    }
+    
+    @IBAction func keyPressedF(_ sender: UIButton) {
+        playSound("F")
+    }
+    
+    @IBAction func keyPressedG(_ sender: UIButton) {
+        playSound("G")
+    }
+    
+    @IBAction func keyPressedA(_ sender: UIButton) {
+        playSound("A")
+    }
+    
+    @IBAction func keyPressedB(_ sender: UIButton) {
+        playSound("B")
+    }
+    
 }
 
